66f2ab5442490ad42c28dec89978eaee580117bc
Katie Morgan <kmorgan1257@yahoo.com>
2015-07-13 10:07:23 -0400

    Changes
    Made a few changes with style and layout.  Still working on image
    carousel for forms docs.

--- app.js
+++ app.js
@@ -20,3 +20,29 @@ $("nav a").each(function() {
 $("nav select").change(function() {
   window.location = $(this).find("option:selected").val();
 });
+
+
+
+
+
+//image carousel
+ $(function() {
+  $('carousel').carouFredSel({
+    width: '100%';
+    items: {
+      visible: 3,
+      start: -1
+    },
+    scroll: {
+      items: 1,
+      duration: 1000,
+      timeoutDuration: 3000
+    },
+    prev: '#prev',
+    next: '#next',
+    pagination: {
+      container: '#pager',
+      deviation: 1
+    }
+  });
+});

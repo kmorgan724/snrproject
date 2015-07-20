f76977f07b2214d745d755a5aea4a8062163f343
Katie Morgan <kmorgan1257@yahoo.com>
2015-07-20 08:50:07 -0400

    Carousel
    Made and image carousel with hrefs for the documents section. Also,
    switched the navbar to a fixed location.

--- app.js
+++ app.js
@@ -26,23 +26,3 @@ $("nav select").change(function() {
 
 
 //image carousel
- $(function() {
-  $('carousel').carouFredSel({
-    width: '100%';
-    items: {
-      visible: 3,
-      start: -1
-    },
-    scroll: {
-      items: 1,
-      duration: 1000,
-      timeoutDuration: 3000
-    },
-    prev: '#prev',
-    next: '#next',
-    pagination: {
-      container: '#pager',
-      deviation: 1
-    }
-  });
-});
